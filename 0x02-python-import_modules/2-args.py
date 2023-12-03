cba1697c4b01c6b03d61bb88e322a9d5be20af39
Abdessattar <ghabi.1602@gmail.com>
2023-12-03 23:01:10 +0000

    Debug the source file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    if len(argv) == 2:
+    import sys
+    if len(sys.argv) == 2:
         print("{} argument:".format(1))
-        print("{}: {}".format(1, argv[1]))
-    elif len(argv) == 1:
+        print("{}: {}".format(1, sys.argv[1]))
+    elif len(sys.argv) == 1:
         print("{} arguments.".format(0))
     else:
-        print("{} arguments:".format(len(argv)))
-        for i in range(1, len(argv)):
-            print("{}: {}".format(i, argv[i]))
+        print("{} arguments:".format(len(sys.argv)))
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
