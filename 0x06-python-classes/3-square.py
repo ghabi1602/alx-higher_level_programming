d0ac0c31ea0500da451f016e1e30f886d2a0c31f
Abdessattar <ghabi.1602@gmail.com>
2023-12-19 09:34:44 -0800

    Update the file

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
-            size (int): The size of the new square.
+            size (int): The size of the new square
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
