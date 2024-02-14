b1cad9dd33d738d7292d2daf4ab592c0d4cdee2a
Abdessattar <ghabi.1602@gmail.com>
2024-02-14 12:51:30 -0800

    Fix code

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,5 @@
 -- create database and user with select privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+USE DATABASE hbtn_0d_2
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
