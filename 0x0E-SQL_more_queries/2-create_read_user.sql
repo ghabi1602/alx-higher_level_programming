56c91a92679e2e5bc79e9b99dc1a0bbe211f995d
Abdessattar <ghabi.1602@gmail.com>
2024-02-14 12:49:22 -0800

    Fix script

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
 -- create database and user with select privilege
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
