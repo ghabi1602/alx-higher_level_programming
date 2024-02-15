0f56d7238cf51d2115aa56343cd5ea7f1f02c30a
Abdessattar <ghabi.1602@gmail.com>
2024-02-15 05:26:38 -0800

    Fix script

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,7 +1,7 @@
 -- lists all shows without genre linked
 SELECT s.`title`, g.`genre_id`
 FROM tv_shows AS s
-	FULL JOIN tv_show_genres AS g
+	LEFT JOIN tv_show_genres AS g
 	ON s.`id` = g.`genre_id`
 	WHERE g.`genre_id` IS NULL
 ORDER BY s.`title`, g.`genre_id`;
