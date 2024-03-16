27d91c4a80180aebd6deb267085eeaf8970f9d1d
Abdessattar <ghabi.1602@gmail.com>
2024-03-16 08:43:23 -0700

    Update repo

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Displays values in states table that matches the argument """
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+
+    cur.execute("""SELECT * FROM {}
+                WHERE {} = {}""".format(states, name, sys.argv[4]))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
