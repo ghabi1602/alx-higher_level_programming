e64518e3ee9f52693a03dcf2492c97463db967c3
Abdessattar <ghabi.1602@gmail.com>
2024-03-17 04:44:44 -0700

    Debug

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -12,8 +12,7 @@ if __name__ == "__main__":
     cur.execute("""SELECT name FROM cities
                 INNER JOIN states
                 ON cities.state_id=states.id
-                WHERE states.name={}
-                ORDER BY cities.id""".format(sys.argv[4]))
+                WHERE states.name=%s""", (sys.argv[4],))
 
     rows = cur.fetchall()
 
