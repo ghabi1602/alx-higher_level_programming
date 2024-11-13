768b4711651224febc2c7b3d685e0be1b6a64044
Abdessattar <ghabi.1602@gmail.com>
2024-11-13 13:57:37 -0800

    Create a class that inherits from Rectangle and prints a square depends on the parameter

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -8,5 +8,16 @@ class Square extends Rectangle {
 			super(size, size);
 		}
 	}
+
+	charPrint (c) {
+		if (c) {
+			for (let i = 0; i < this.height; i++) {
+				console.log(c.repeat(this.width));
+			}
+		}
+		else {
+			this.print()
+		}
+	}
 }
 module.exports = Square;
